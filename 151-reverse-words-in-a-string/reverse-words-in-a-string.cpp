94b9625eb2def7249c62d9d90a408fddf0840a71
Synergicvans <91491055+Synergicvans@users.noreply.github.com>
2025-07-02 11:17:13 +0530

    Time: 17 ms (7.06%) | Memory: 11.3 MB (31.70%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string>ans;
         string temp="";
-        
+        vector<string>str;
         for(char c:s){
-            if(c!=' '){
-                temp+=c;
-            }
-            else if(!temp.empty()){
-                ans.push_back(temp);
+            
+            if(c!=' ') temp+=c;
+            else{
+                if(!temp.empty()) str.push_back(temp);
                 temp="";
-            }
+            };
+        }
+        //
+        for(auto c:str){
+            cout<<c<<" ";
+        }
+        //
+        if(!temp.empty()){
+            str.push_back(temp);
+            temp="";
+        }
 
+        for(int i=0;i<str.size()/2;i++){
+            swap(str[i],str[str.size()-i-1]);
         }
-        if(!temp.empty()) ans.push_back(temp);
-        temp="";
-        //
-        for(auto i:ans){
-            cout<<i;
-        }
-        cout<<endl;
-        //
-        for(int i=0;i<ans.size()/2;i++){
-            swap(ans[i],ans[ans.size()-i-1]);
-        }
-        //
-        for(auto i:ans){
-            cout<<i;
-        }
-        //
-        for(int i=0;i<ans.size();i++){
-            temp+=ans[i];
-            if(i!=ans.size()-1) temp+=" ";
+
+        for(int i=0;i<str.size();i++){
+            temp+=str[i];
+            if(i!=str.size()-1) temp+=' ';
         }
         return temp;
     }
