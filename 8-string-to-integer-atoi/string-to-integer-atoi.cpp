2ed46894099cc3f24d868f026474de6ac9ac1738
Synergicvans <91491055+Synergicvans@users.noreply.github.com>
2025-07-07 18:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (56.54%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0,n=s.size(),num=0,sign=1;
-        while(i<n && s[i]==' ') i++;
-
-        if(s[i]=='-' || s[i]=='+'){
+        int i=0,n=s.size(),sign=1;
+        long ans=0;
+        while(s[i]==' ' && i<n){
+            i++;
+        }
+        if(i<n && s[i]=='-' || s[i]=='+'){
             if(s[i]=='-') sign=-1;
             i++;
         }
-        while(i<n){
-            int digit =s[i]-'0';
-            if(digit<0 || digit >10) break;
-
-            // Step 4: Check for overflow
-            if (num > (2147483647 / 10) || (num == 2147483647 / 10 && digit > 7)) {
-                if (sign == 1) return 2147483647;
-                else return -2147483648;
-            }
-            num=num*10+digit;
+        while(s[i]>='0' && s[i]<='9' && i<n){
+            int digit=s[i]-'0';
+            ans=ans*10+digit;
+            if(sign*ans>INT_MAX) return INT_MAX;
+            if(sign*ans<INT_MIN) return INT_MIN;
             i++;
         }
-        return num*sign;
+        return ans*sign;
     }
 };
