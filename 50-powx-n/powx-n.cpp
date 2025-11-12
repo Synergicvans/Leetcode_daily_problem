b0b55d5eb4ca8a858f947ae52e9981274f08a92e
Synergicvans <91491055+Synergicvans@users.noreply.github.com>
2025-11-12 22:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (14.50%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    double myPow(double x, int y) {
-        long n=y;
+    double myPow(double x, long long n) {
+
         double returns =1;
         if(x==1) return 1;
         else if(x==-1){
             if(n%2==0) return 1;
+            else return -1;
         }
         if(n>0){
             while(n>0){
