bcd8dc40ef90925595da8dc60f0a516ece42b4bc
Synergicvans <91491055+Synergicvans@users.noreply.github.com>
2025-12-28 00:53:59 +0530

    Time: 451 ms (59.03%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,7 @@
 SELECT today.id
 FROM Weather yesterday 
-CROSS JOIN Weather today
+JOIN Weather today
 
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+on DATEDIFF(today.recordDate,yesterday.recordDate) = 1
     AND today.temperature > yesterday.temperature
 ;
