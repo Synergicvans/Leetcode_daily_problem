bd8605c00d9d9972b1eb78c2a76c0e9db5c902ac
Synergicvans <91491055+Synergicvans@users.noreply.github.com>
2026-03-31 22:22:38 +0530

    Time: 16 ms (7.24%) | Memory: 15.3 MB (5.49%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode* temp=head;
+        
+        unordered_map<ListNode*,int>mpp;
+
+        while(temp!=nullptr){
+            mpp[temp]++;
+            if(mpp[temp]==2) return true;
+            temp=temp->next;
+        }
+        return false;
+        
+    }
+};
